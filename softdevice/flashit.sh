27a98fc3f6611ad32ef23b6d3254ff03489b9289
Brian Beattie <beattie@beattie-home.net>
2025-10-16 10:12:05 -0700

    Add fetch and flash nRF softdevice firmware

---
+++ softdevice/flashit.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+probe-rs download --chip nRF52840 --protocol SWD s140_nrf52_7.3.0.hex
---
+++ softdevice/getit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "If this download fails check the website https://www.nordicsemi.com/Products/Development-software/S140/Download"
+wget https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/softdevices/s140/s140_nrf52_7.3.0.zip
+
